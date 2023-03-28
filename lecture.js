d820fa751d622bbd2f2d51763f58477825f3026e
quincyp <quincytpham@gmail.com>
2023-03-28 16:59:09 -0700

    lecture: camel case, naming convenetion, reserved words, comments, console log, declaring variables, data types

--- lecture.js
+++ lecture.js
@@ -5,10 +5,37 @@ Make sure to code-along during lecture.
 
 
 // 1. Link the script.js file from the HTML
+    // see html file for script tag
+
 
 // 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
+    // camelCaseLooksLikeThis
+
 
 // 3. Creating Variables + Data Types
+    // Changing the value of a variable
+    let text = "this is a sentence";
+
+    // Placing the variable into a function()
+    alert(text);
+    console.log(text);
+
+    // 7 Primitive Data Types
+        /*
+        String = text
+        Number = number
+        Bigint = really big number
+        Boolean = true or false
+        Undefined = value is undefined until you set one
+        Null = there is no value
+        Symbol = symbol
+        */
+    // 3 Object Data Types
+        /*
+        object = unindexed list that uses key:value pairs
+        array = indexed list of values
+        date = date
+        */
 
 // 4. Creating Functions (+ Event Listeners?)
 
@@ -23,4 +50,3 @@ Make sure to code-along during lecture.
 // 9. DOM Manipulation
 
 // 10. Events
-
