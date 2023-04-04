1bac625ca5e37b4daeca8641bc245ba479dc0176
quincyp <quincytpham@gmail.com>
2023-04-04 12:10:51 -0700

    added numbers and strings lesson

--- lecture.js
+++ lecture.js
@@ -38,13 +38,69 @@ Make sure to code-along during lecture.
         */
 
 // 4. Creating Functions (+ Event Listeners?)
-    // Declaring functions
+    // REVIEW:
+        // 3 TYPES OF VARIABLES:
+        var a;
+        let b;
+        const c =  3;
 
+        a = 1;
+        b = 2;
+
+        a = 11;
+        a = 12;
+        a = a + 1; // 13
+
+        // console.log(a);
+
+
+    // Function Statements
+    let x = 1;
+    function hello() {
+        console.log("hello world");
+        // addition(5,7);
+        console.log(x);
+    }
+    
+    function addition(a, b) {
+                //5   7   1
+        let sum = a + b + x;
+        console.log(sum);
+        // return sum;
+    }
+
+    // Function Expression
+    let add = function addition2(a, b) {
+        let sum = a + b;
+        console.log(sum);
+    }
     // Invoking functions
+    hello();
+    addition(5, 7);
+    // add(5,7);
+
 
     // Scope local and global
 
+
+    // Example Problem: we want to make a function that gives us the area given the length and width
+    function area(length, width) {
+        return length * width;
+    }
+    console.log(area(3,5));
+
+    // Example Problem 2: we are given the area function, and now we want to compute volume given a height
+    const volume = function(length, width, height) {
+        console.log(height * area(length,width));
+        return height * area(length,width);
+    }
+
+    volume(3, 5, 10);
+
     // Parameters as input
+
+
+    // Arrow Functions
 
 // 5. Strings
 
--- student.js
+++ student.js
@@ -20,24 +20,56 @@ Complete the following numbered tasks:
     // 4. Assign one to the type String below:
         person = "Quincy Pham";
 
-console.log("This is the bankaccount console log: " + bankAccount);
-console.log(bankAccount - bills);
-console.log(rich);
-console.log(person);
+// console.log("This is the bankaccount console log: " + bankAccount);
+// console.log(bankAccount - bills);
+// console.log(rich);
+// console.log(person);
 
-console.log(person + " is rich, this is " + rich + " because they have this much $" + bankAccount )
+// console.log(person + " is rich, this is " + rich + " because they have this much $" + bankAccount )
 
 
-let student = "christy";
-console.log(student);
-student = ava;
-console.log(student);
+// let student = "christy";
+// console.log(student);
+// student = ava;
+// console.log(student);
 
 /*
 ========== LESSON 2 - FUNCTIONS ==========
 */
-// 1. Create a doubleNumber function that takes in 1 number parameter and returns double the value.
+// 1. Create a doubleNumber function statement that takes in 1 number parameter and returns double the value.
 
-// 2. Create a squareNumber function that takes in 1 number parameter and returns the square
+// 2. Create a squareNumber function expression that takes in 1 number parameter and returns the square
 
-// 3. Create a productNumber function that takes in 2 number parameters and returns the product of the 2 numbers
+// 3. Create a productNumber arrow function that takes in 2 number parameters and returns the product of the 2 numbers
+
+
+
+
+/*
+========== LESSON 3 - NUMBERS ==========
+*/
+// 1. Create a sumOfThree function statement that takes in 3 number parameter and returns sum.
+
+// 2. Create a productofThree function expression that takes in 3 number parameter and returns the quotient.
+
+// 3. Create a getRemainder arrow function that takes in 2 number parameters and returns the remainder of the first number divided by the second number
+
+// 4. Create a getRootRounded function that takes in 1 number, and returns the absolute value's square root, rounded up. Note: negative numbers should have their absolute value taken to still work.
+
+// 5. Create a getRandom function that returns a random number from 1-100.
+
+
+
+/*
+========== LESSON 4 - STRINGS ==========
+*/
+// 1. Create a getPersonStats function statement that takes in 3 Strings: name, job, age. The function should console log a statement such as "Hi my name is [NAME], I am [AGE] years old, and I am a [JOB]".
+
+// 2. Create a sayFirstAndLast function expression that takes in 1 String parameter and console logs "the first character is: [firstChar] and last character is: [lastChar]".
+
+// 3. Create a yellFruitNum arrow function that takes in 1 number parameter, 1 String that is a fruit parameter, and console logs the sentence "I have [number] [FRUIT]" in all caps.
+
+// 4. Create a sliceFruit function that takes in a String of a fruit and console logs everything except the first 2 letters.
+
+// 5. Create a splitFruit function that takes in a String of a fruit and returns an array of the letters split into each index of the array.
+
